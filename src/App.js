4cc5e056f395de218c4c92d7dce228a6430b992f
Shervin <shervink213@gmail.com>
2021-12-28 15:21:19 -0500

    added the post command to get auth token

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import React from "react";
 
 
 
-//const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = process.env.REACT_APP_API_KEY;
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-//const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 const authURL = `https://www.bungie.net/en/OAuth/Authorize?client_id=${CLIENT_ID}&response_type=code`
 let authCode = undefined;
 
@@ -19,15 +19,17 @@ const getCode = () => {
 if (window.location.href.includes('code=')){
   var queryString = window.location.search;
   let params = new URLSearchParams(queryString);
-  authCode = parseInt(params.get('code'));
+  authCode = params.get('code');
   console.log(authCode);
+  getToken();
 }
 
-/*
+
 const getToken = () => {
   fetch("https://www.bungie.net/Platform/App/OAuth/token/", {
     method: 'POST',
     headers: {
+      'X-API-Key': apiKey,
       'Content-Type': 'application/x-www-form-urlencoded',
       'Authorization': `Basic ${window.btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`
     },
@@ -41,7 +43,7 @@ const getToken = () => {
     return Response.json();
   })
 }
-*/
+
 
 
 
