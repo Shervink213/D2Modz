3d2b1857627ddc59371b1c314fa6f7d3199639b5
Shervin <shervink213@gmail.com>
2021-12-28 14:51:18 -0500

    testing stuff with authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React ,{ useEffect } from "react";
+//import fetch from 'isomorphic-fetch';
+
+
+
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+const authURL = `https://www.bungie.net/en/OAuth/Authorize?client_id=${CLIENT_ID}&response_type=code`
+const authCode = undefined;
+
+
+const getCode = () => {
+  window.location.href = authURL;
+  
+}
+
+if (window.location.href.includes('code=')){
+  var queryString = window.location.href.search;
+  let params = new URLSearchParams(queryString);
+  authCode = parseInt(params.get('code'));
+  console.log(authCode);
+}
+
+const getToken = () => {
+  fetch("https://www.bungie.net/Platform/App/OAuth/token/", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Basic ${window.btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`
+    },
+    body: new URLSearchParams({
+      'client_id': `${CLIENT_ID}`,
+      'grant_type': 'authorization_code',
+      'code': authCode
+    }).toString()
+  }).then(Response = () => {
+    console.log(Response);
+    return Response.json();
+  })
+}
 
 
 
 const App = () => {
 
+
+  
   return (
     <div>
       <h1>Placeholder</h1>
-      
+      <button id="myAnchor">
+        get token
+      </button>
     </div>
   )
 }
